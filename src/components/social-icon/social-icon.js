7f1c10b9198ecf3ee7a3351309a2cee173cabc95
ksenia <30226190+ksenius@users.noreply.github.com>
2020-05-09 00:37:40 +0700

    Fix gradient icon bug in Firefox

--- src/components/social-icon/social-icon.js
+++ src/components/social-icon/social-icon.js
@@ -1,1 +1,11 @@
 import './social-icon.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const svgSprite = document.body.firstElementChild;
+  const defs = svgSprite.querySelectorAll('defs');
+
+  defs.forEach((item) => {
+    svgSprite.prepend(item.cloneNode(true));
+    item.remove();
+  });
+});
